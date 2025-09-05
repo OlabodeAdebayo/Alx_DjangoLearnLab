9cfdc7976ddcb19c6c4784381ba391cb25405d13
OlabodeAdebayo <adesamson0404@gmail.com>
2025-09-05 16:27:46 -0700

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+
+def list_books(request):
+    # Retrieves all books and renders a template displaying the list.
+    books = Book.objects.all()
+    context = {'list_books': books}
+
+    return render(request, 'books/list_books.html', context)
+
+class LibraryDetailView(Detailview):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.objects.books.all()
+        return context
