3203c7f20bc580275c4510e544d00f783ececc19
OlabodeAdebayo <adesamson0404@gmail.com>
2025-09-05 21:16:30 -0700

    new update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_books
+from  .views import list_books, LibraryDetailView
 
 urlpatterns = [
-        path('books/', list_books, name='list_books'),
-        path('library/<int:pk>/', views.`:wqLibraryDetailView.as_view(), name='library_detail')
+    path("books/", views.list_books, name="list_books"),  # FBV
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # CBV
+]
