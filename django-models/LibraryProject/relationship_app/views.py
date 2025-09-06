537df6d98180fd1b28202335352e0592c886fd1e
OlabodeAdebayo <adesamson0404@gmail.com>
2025-09-05 17:13:11 -0700

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Library
+from .models import Library, Book
 
 
 def list_books(request):
     # Retrieves all books and renders a template displaying the list.
     books = Book.objects.all()
-    response_text = "\n".join([f"{book.title} by {book.author.name}" for book in books])
 
-
-    return HttpResponse(response_text, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(Detailview):
     model = Library
@@ -19,5 +17,5 @@ class LibraryDetailView(Detailview):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = self.objects.books.all()
+        context['books'] = self.object.books.all()
         return context
