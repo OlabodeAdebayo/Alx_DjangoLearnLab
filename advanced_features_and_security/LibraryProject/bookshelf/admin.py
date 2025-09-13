c2c05c142db0846760d0e0c6654015ed2e21197d
OlabodeAdebayo <adesamson0404@gmail.com>
2025-09-12 21:17:50 -0700

    new

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,23 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Book
+from .models import Book, CustomUser
 
 
 # Extend UserAdmin to include custom fields for CustomUser
-class UserAdmin(UserAdmin):
-    model = UserAdmin
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
     list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active", "date_of_birth")
+    list_filter = ("is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active")}
+        ),
+    )
+
     search_fields = ("username", "email")
-
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {"fields": ("date_of_birth", "profile_photo")}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {"fields": ("date_of_birth", "profile_photo")}),
-    )
-
-
+    ordering = ("username",)
+    
+    
+# Register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # Keep your existing Book admin
 @admin.register(Book)
