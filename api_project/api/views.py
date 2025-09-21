08e77aac1e32d8d2ae67d4b73ae98a20c647f71f
OlabodeAdebayo <adesamson0404@gmail.com>
2025-09-20 18:27:54 -0700

    CRUD operation

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()       # Use the Book model as the queryset
     serializer_class = BookSerializer
-# Create your views here.
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()         # Retrieve all Book records
+    serializer_class = BookSerializer
