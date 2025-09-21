828e3192423c0147e47328d7f914f98c5a39af7e
OlabodeAdebayo <adesamson0404@gmail.com>
2025-09-20 18:43:42 -0700

    authentication and permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,3 +10,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()         # Retrieve all Book records
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] # Only authenticated users can access
