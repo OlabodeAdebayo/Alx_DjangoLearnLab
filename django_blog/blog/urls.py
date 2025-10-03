41437b6449ce61f0f44841532aee774190fe8167
OlabodeAdebayo <adesamson0404@gmail.com>
2025-10-02 21:47:22 -0700

    post_view

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("register/", views.register_view, name="register"),
     path("profile/", views.profile_view, name="profile"),
-    path('posts/', views.PostListView.as_view(), name='post-list'),         # /posts/
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'), # /posts/new/
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'), # /posts/<pk>/
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'), # /posts/<pk>/edit/
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'), # /posts/<pk>/delete/
+    path('posts/', PostListView.as_view(), name='post-list'),         # /posts/
+    path('posts/new/', PostCreateView.as_view(), name='post-create'), # /posts/new/
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # /posts/<pk>/
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'), # /posts/<pk>/edit/
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # /posts/<pk>/delete/
 ]
