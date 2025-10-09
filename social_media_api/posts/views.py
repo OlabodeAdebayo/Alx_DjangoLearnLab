ba2f9496f5dfd0b3ac1da69f62019beb8764a4bd
OlabodeAdebayo <adesamson0404@gmail.com>
2025-10-09 12:09:16 -0700

    views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -38,5 +38,4 @@ class FeedView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         followed_users = user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
-
+        return Post.objects.filter(author__in=followed_users).order_by
