0bafc12ecfd1b6f32246d33c94e0b6a678c863c4
OlabodeAdebayo <adesamson0404@gmail.com>
2025-10-09 12:45:56 -0700

    posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics, viewsets, permissions, filters
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from .models import Post, Comment, Like
